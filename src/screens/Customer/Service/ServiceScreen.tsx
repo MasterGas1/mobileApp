c21cbf6f58208262937c1abb293323fbb79ac4e3
ManuelBarba06 <113741996+ManuelBarba06@users.noreply.github.com>
2024-04-08 10:32:15 -0600

    221_MABG_FEAT_Add design service customer

--- src/screens/Customer/Service/ServiceScreen.tsx
+++ src/screens/Customer/Service/ServiceScreen.tsx
@@ -1,12 +1,86 @@
 import React from 'react'
-import { View, Text} from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions, Linking, FlatList} from 'react-native'
+
+import Spacer from '../../../components/common/Spacer'
+
+import { globalColors } from '../../../styles/globalVariables'
+import { useService } from '../../../hooks/useService'
+import { ActivityIndicator } from 'react-native-paper'
+import ServiceButton from '../../../components/ServiceButton'
 
 const ServiceScreen = () => {
+
+  const {services, isLoading} = useService();
+
+  const handleClickAdd = () => {
+    Linking.openURL('https://mastergas23.com/');
+  }
+
   return (
-    <View>
-        <Text>Service Screen</Text>
+    <View style={styles.container}>
+        
+        <TouchableOpacity 
+          style={styles.containerAdd}
+          onPress={handleClickAdd}  
+        >
+            <Image
+              source={require('../../../../assets/Logo.png')}
+              style={styles.imageLogo}
+            />
+            <Text style={styles.textCompany}>Visita nuestra página</Text>
+        </TouchableOpacity>
+
+        <Spacer/>
+        {
+          isLoading
+          ? <ActivityIndicator size={'large'} color={globalColors.principalColor}/>
+          : <FlatList
+              style={styles.containerList}
+              data={services}
+              renderItem={({item}) => <ServiceButton
+                name={item.name}
+                description={item.description}
+                image={item.image}
+                id={item._id}
+              />}
+              keyExtractor={item => item._id}
+            />
+        }
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20
+  },
+  containerAdd: {
+    backgroundColor: globalColors.principalColor,
+    borderRadius: 10,
+    width: '100%',
+    height: '30%',
+    padding: 10,
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  imageLogo: {
+    width: "50%",
+    resizeMode: "stretch",
+    height: "100%"
+  },
+  textCompany: {
+    color: 'white',
+    fontSize: Dimensions.get('window').width * 0.05,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: '45%'
+  },
+  containerList: {
+    flex: 2,
+    width: '100%',
+  }
+})
 
 export default ServiceScreen
