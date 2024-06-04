4ff8246df2679c21d5fcd2f89d4a4a541e24a486
AlexeiNay <alexeisanchez66@gmail.com>
2024-06-03 19:18:57 -0700

    Profile screen and custom component options

--- src/screens/Customer/Profile/ProfileScreen.tsx
+++ src/screens/Customer/Profile/ProfileScreen.tsx
@@ -1,12 +1,76 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { globalColors } from '../../../styles/globalVariables'
+import CustomOption from '../../../components/CustomOption';
+interface OptionProps {
+  icon: string;
+  text: string;
+}
 
 const ProfileScreen = () => {
+
   return (
-    <View>
-        <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.header} />
+      <View style={styles.profileSection}>
+        <Image
+          source={{ uri: 'https://avatarfiles.alphacoders.com/693/thumb-1920-69306.jpg' }} // Reemplaza con la URL de la imagen de perfil
+          style={styles.profileImage}
+        />
+        <TouchableOpacity style={styles.editIconContainer}>
+          <Ionicons name="pencil-outline" size={25} color={'white'} />
+        </TouchableOpacity>
+        <Text style={styles.profileName}>Adela Micha</Text>
+      </View>
+      <View style={styles.optionsSection}>
+        <CustomOption icon="pencil-outline" text="Editar perfil" />
+        <CustomOption icon="key" text="Cambiar contraseña" />
+        <CustomOption icon="exit" text="Cerrar sesión" />
+      </View>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    height: 240,
+    backgroundColor: globalColors.principalColor,
+  },
+  profileSection: {
+    alignItems: 'center',
+    marginTop: -75,
+  },
+  profileImage: {
+    width: 160,
+    height: 160,
+    borderRadius: 75,
+    borderWidth: 4,
+    borderColor: 'white',
+  },
+  editIconContainer: {
+    position: 'absolute',
+    right: 130,
+    bottom: 40,
+    backgroundColor: '#004d40',
+    borderRadius: 50,
+    padding: 5,
+  },
+  profileName: {
+    marginTop: 10,
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#004d40',
+  },
+  optionsSection: {
+    marginTop: 30,
+    marginHorizontal: 20,
+  },
+});
+
 
 export default ProfileScreen
